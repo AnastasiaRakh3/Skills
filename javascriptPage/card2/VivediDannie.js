fc68c14f23ee784f6e17374ceabeed25774ec3b3
AnastasiaRakh <rakhmatova-93@mail.ru>
2020-05-20 13:10:12 +0300

    Добавление формы

---
+++ javascriptPage/card2/VivediDannie.js
@@ -0,0 +1,9 @@
+let message = document.querySelector('.subscription-message');
+let email = document.querySelector('.subcription-email');
+let form = document.querySelector('.subscription');
+
+form.onsubmit = function(evt) {
+    evt.preventDefault();
+    message.textContent = 'Адрес ' + email.value + 'добавлен в список получателей рассылки!(Шучу, не добавлен:)';
+};
+
