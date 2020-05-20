f7b9927cca2aae7f0f65c14e553be0e93b4d74ab
AnastasiaRakh <rakhmatova-93@mail.ru>
2020-05-20 13:16:39 +0300

    Добавление адреса в action

--- javascriptPage/card2/VivediDannie.js
+++ javascriptPage/card2/VivediDannie.js
@@ -1,9 +1,11 @@
-let message = document.querySelector('.subscription-message');
-let email = document.querySelector('.subcription-email');
-let form = document.querySelector('.subscription');
+let message = document.querySelector(".subscription-message");
+let email = document.querySelector(".subcription-email");
+let form = document.querySelector(".subscription");
 
-form.onsubmit = function(evt) {
-    evt.preventDefault();
-    message.textContent = 'Адрес ' + email.value + 'добавлен в список получателей рассылки!(Шучу, не добавлен:)';
+form.onsubmit = function (evt) {
+  evt.preventDefault();
+  message.textContent =
+    "Адрес " +
+    email.value +
+    "добавлен в список получателей рассылки!(Шучу, не добавлен:)";
 };
-
