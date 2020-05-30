659f83be386c859b1f8f3b6f82f0ac665a248bee
AnastasiaRakh <rakhmatova-93@mail.ru>
2020-05-30 17:25:23 +0300

    Новая карточка Прокрутка

--- javascriptPage/card2/VivediDannie.js
+++ javascriptPage/card2/VivediDannie.js
@@ -1,13 +1,13 @@
-let message = document.querySelector(".subscription-message");
-let email = document.querySelector(".subcription-email");
-let form = document.querySelector(".subscription");
+let message = document.querySelector('.subscription-message');
+let email = document.querySelector('.subcription-email');
+let form = document.querySelector('.subscription');
 
 form.onsubmit = function (evt) {
   evt.preventDefault();
   message.textContent =
-    "Адрес " +
+    'Адрес ' +
     email.value +
-    " добавлен в список получателей рассылки! Шучу, не добавлен:)";
+    ' добавлен в список получателей рассылки! Шучу, не добавлен:)';
     email.value = '';
     
 };
---
+++ javascriptPage/card9/ProkrutiStranitsu.js
@@ -0,0 +1,12 @@
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 400) {
+    upButton.classList.remove('hidden');
+  } else {
+    upButton.classList.add('hidden');
+  }
+};
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
